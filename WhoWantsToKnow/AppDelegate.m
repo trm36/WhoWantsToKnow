f14999ca5092954cb6f4c98a1306231b89d9db87
Taylor Mott <trm36@humboldt.edu>
2014-09-23 18:01:11 -0700

    Step 1: Add a new ViewController as rootVC
    - Create a new root view controller (you don't need an XIB file)
    - Add the view controller in nav controller to the rootViewController of the window
    
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

--- WhoWantsToKnow/AppDelegate.m
+++ WhoWantsToKnow/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "WWTKViewController.h"
 
 @implementation AppDelegate
 
@@ -14,6 +15,12 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    WWTKViewController *wwtkVC = [WWTKViewController new];
+    
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:wwtkVC];
+    
+    self.window.rootViewController = navController;
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ WhoWantsToKnow/WWTKViewController.h
@@ -0,0 +1,13 @@
+//
+//  WWTKViewController.h
+//  WhoWantsToKnow
+//
+//  Created by Taylor Mott on 9.23.14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WWTKViewController : UIViewController
+
+@end
